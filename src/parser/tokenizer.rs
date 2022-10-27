2176ced32b6fc945f826bc7f5a6d218d476a041d
Jonathan Delgado <hi@jon.soy>
2022-10-26 23:11:40 -0300

    feat: new tokenizer

--- src/parser/tokenizer.rs
+++ src/parser/tokenizer.rs
@@ -1,4 +1,7 @@
-use std::{ops::Add, usize, vec};
+use std::{
+    ops::{Add, RangeBounds, RangeInclusive, RangeToInclusive},
+    usize, vec,
+};
 
 #[derive(Debug)]
 pub struct Span {
@@ -30,22 +33,18 @@ struct Cursor {
 }
 
 impl Cursor {
-    fn new<A>(index: usize, payload: A) -> Self
-    where
-        A: ToString,
-    {
-        Self {
-            payload: payload.to_string(),
-            index,
+    fn current_char(&self) -> Option<char> {
+        self.payload.chars().nth(self.index)
+    }
+
+    fn current_char_expected(&self, val: char) {
+        if !self.current_matches_char(val) {
+            todo!(
+                "Require error expected the char {} (ascii code {})",
+                val,
+                val as usize
+            );
         }
-    }
-
-    fn has_next<'a>(&'a self) -> bool {
-        (self.payload.len() > (self.index + 1)).clone()
-    }
-
-    fn has_current<'a>(&'a self) -> bool {
-        self.payload.len() > self.index
     }
 
     fn current_matches_char(&self, val: char) -> bool {
@@ -56,6 +55,61 @@ impl Cursor {
         }
     }
 
+    pub fn current_matches_range_char<T>(&self, vec_ranges: &Vec<T>) -> bool
+    where
+        T: RangeBounds<char> + std::fmt::Debug,
+    {
+        let current_char = self.current_char();
+        if let Some(current_char) = current_char {
+            for e in vec_ranges {
+                if e.contains(&current_char) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    fn current_range_char_expected<T>(&self, vec_ranges: &Vec<T>)
+    where
+        T: RangeBounds<char> + std::fmt::Debug,
+    {
+        if !self.current_matches_range_char(&vec_ranges) {
+            todo!("Require error expected the char {:?}", &vec_ranges);
+        }
+    }
+
+    fn forward(&mut self, positions: usize) {
+        self.index = self.index + positions;
+    }
+
+    fn get_by_span(&self, span: &Span) -> String {
+        unsafe { self.payload.get_unchecked(span.start..span.end).to_string() }
+    }
+
+    fn has_current<'a>(&'a self) -> bool {
+        self.payload.len() > self.index
+    }
+
+    fn has_next<'a>(&'a self) -> bool {
+        (self.payload.len() > (self.index + 1)).clone()
+    }
+
+    fn new<A>(index: usize, payload: A) -> Self
+    where
+        A: ToString,
+    {
+        Self {
+            payload: payload.to_string(),
+            index,
+        }
+    }
+
+    fn next_char(&self) -> Option<char> {
+        self.payload.chars().nth(self.index + 1)
+    }
+
     fn next_matches_char(&self, val: char) -> bool {
         if let Some(c) = self.next_char() {
             c == val
@@ -64,34 +118,20 @@ impl Cursor {
         }
     }
 
-    fn next_matches_range_char(&self, range: std::ops::RangeInclusive<char>) -> bool {
-        matches!(self.next_char(), range)
-    }
-
-    fn next_char(&self) -> Option<char> {
-        self.payload.chars().nth(self.index + 1)
-    }
-
-    fn current_char(&self) -> Option<char> {
-        self.payload.chars().nth(self.index)
-    }
-
-    fn forward(&mut self, positions: usize) {
-        self.index = self.index + positions;
-    }
-
-    fn current_char_expected(&self, val: char) {
-        if let Some(c) = self.current_char() {
-            if c != val {
-                todo!("Require error expected the char {}", val);
+    pub fn next_matches_range_char<T>(&self, vec_ranges: &Vec<T>) -> bool
+    where
+        T: RangeBounds<char> + std::fmt::Debug,
+    {
+        let current_char = self.next_char();
+        if let Some(current_char) = current_char {
+            for e in vec_ranges {
+                if e.contains(&current_char) {
+                    return true;
+                }
             }
-        } else {
-            todo!("Require error: cannot found current char");
         }
-    }
 
-    fn get_by_span(&self, span: &Span) -> String {
-        unsafe { self.payload.get_unchecked(span.start..span.end).to_string() }
+        return false;
     }
 }
 
@@ -116,7 +156,37 @@ impl Tokenizer {
             }
 
             if cursor.current_matches_char('\n') {
-                tokens.extend(Self::parse_new_line(cursor));
+                tokens.extend(Self::parse_newline(cursor));
+                continue;
+            }
+
+            if cursor.current_matches_range_char(&vec![' '..=' ', '\t'..='\t']) {
+                tokens.extend(Self::parse_spaces(cursor));
+                continue;
+            }
+
+            if cursor.current_matches_char(':') {
+                tokens.extend(Self::parse_colon(cursor));
+                continue;
+            }
+
+            if cursor.current_matches_char('=') {
+                tokens.extend(Self::parse_equal(cursor));
+                continue;
+            }
+
+            if cursor.current_matches_char('"') {
+                tokens.extend(Self::parse_string(cursor));
+                continue;
+            }
+
+            if cursor.current_matches_range_char(&vec!['a'..='z', 'A'..='Z', '_'..='_']) {
+                tokens.extend(Self::parse_keyword(cursor));
+                continue;
+            }
+
+            if cursor.current_matches_range_char(&vec!['0'..='9']) {
+                tokens.extend(Self::parse_number(cursor));
                 continue;
             }
 
@@ -148,7 +218,7 @@ impl Tokenizer {
         vec![comment_token]
     }
 
-    fn parse_new_line(cursor: &mut Cursor) -> Vec<Token> {
+    fn parse_newline(cursor: &mut Cursor) -> Vec<Token> {
         cursor.current_char_expected('\n');
         let span_start = cursor.index;
         cursor.forward(1);
@@ -157,6 +227,134 @@ impl Tokenizer {
             end: cursor.index,
         };
         let newline_token = Token::new("newline", &cursor, span);
+        vec![newline_token]
+    }
+
+    fn parse_spaces(cursor: &mut Cursor) -> Vec<Token> {
+        cursor.current_range_char_expected(&vec![' '..=' ', '\t'..='\t']);
+        let span_start = cursor.index;
+        while cursor.has_current() {
+            if cursor.current_matches_range_char(&vec![' '..=' ', '\t'..='\t']) {
+                cursor.forward(1);
+                continue;
+            }
+            break;
+        }
+        let span = Span {
+            start: span_start,
+            end: cursor.index,
+        };
+        let newline_token = Token::new("space", &cursor, span);
+        vec![newline_token]
+    }
+
+    fn parse_equal(cursor: &mut Cursor) -> Vec<Token> {
+        cursor.current_char_expected('=');
+        let span_start = cursor.index;
+        cursor.forward(1);
+        let span = Span {
+            start: span_start,
+            end: cursor.index,
+        };
+        let newline_token = Token::new("equal", &cursor, span);
+        vec![newline_token]
+    }
+
+    fn parse_colon(cursor: &mut Cursor) -> Vec<Token> {
+        cursor.current_char_expected(':');
+        let span_start = cursor.index;
+        cursor.forward(1);
+        let span = Span {
+            start: span_start,
+            end: cursor.index,
+        };
+        let newline_token = Token::new("colon", &cursor, span);
+        vec![newline_token]
+    }
+
+    fn parse_string(cursor: &mut Cursor) -> Vec<Token> {
+        cursor.current_char_expected('"');
+        cursor.forward(1);
+        let span_start = cursor.index;
+
+        while cursor.has_current() {
+            if cursor.current_matches_char('\\') {
+                cursor.forward(2);
+                continue;
+            }
+            if cursor.current_matches_char('"') {
+                break;
+            }
+            cursor.forward(1);
+        }
+
+        cursor.current_char_expected('"');
+
+        let span = Span {
+            start: span_start,
+            end: cursor.index,
+        };
+        cursor.forward(1);
+        let newline_token = Token::new("string", &cursor, span);
+        vec![newline_token]
+    }
+
+    fn parse_keyword(cursor: &mut Cursor) -> Vec<Token> {
+        cursor.current_range_char_expected(&vec!['a'..='z', 'A'..='Z', '0'..='9', '_'..='_']);
+        let span_start = cursor.index;
+
+        while cursor.has_current() {
+            if !cursor.current_matches_range_char(&vec!['a'..='z', 'A'..='Z', '0'..='9', '_'..='_'])
+            {
+                break;
+            }
+            cursor.forward(1);
+        }
+
+        let span = Span {
+            start: span_start,
+            end: cursor.index,
+        };
+        let newline_token = Token::new("keyword", &cursor, span);
+        vec![newline_token]
+    }
+
+    fn parse_number(cursor: &mut Cursor) -> Vec<Token> {
+        cursor.current_range_char_expected(&vec!['0'..='9']);
+        let span_start = cursor.index;
+        let mut decimal = false;
+
+        while cursor.has_current() {
+            if cursor.current_matches_range_char(&vec!['0'..='9']) {
+                cursor.forward(1);
+                continue;
+            }
+            if cursor.current_matches_char('_') {
+                if !cursor.next_matches_range_char(&vec!['0'..='9']) {
+                    todo!("Only one undesrcore is allowed as numeric separator");
+                }
+                cursor.forward(1);
+                continue;
+            }
+            if cursor.current_matches_char('.') {
+                if decimal {
+                    todo!("Unexpected number");
+                }
+                if !cursor.next_matches_range_char(&vec!['0'..='9']) {
+                    todo!("Invalid or unexpected token");
+                }
+                decimal = true;
+                cursor.forward(1);
+                continue;
+            }
+            break;
+        }
+
+        let span = Span {
+            start: span_start,
+            end: cursor.index,
+        };
+        let newline_token = Token::new("number", &cursor, span);
         vec![newline_token]
     }
 }
--- tests/tokenizer_test.rs
+++ tests/tokenizer_test.rs
@@ -24,4 +24,104 @@ mod tokenizer {
         let tokens = Tokenizer::parse(payload);
         assert_debug_snapshot!(tokens);
     }
+
+    #[test]
+    fn tokenizer_keyword() {
+        let payload = "ABC123_32";
+        let tokens = Tokenizer::parse(payload);
+        assert_debug_snapshot!(tokens);
+    }
+
+    #[test]
+    fn tokenizer_keyword_and_newline() {
+        let payload = "ABC123_32\n";
+        let tokens = Tokenizer::parse(payload);
+        assert_debug_snapshot!(tokens);
+    }
+
+    #[test]
+    fn tokenizer_colon() {
+        let payload = ":";
+        let tokens = Tokenizer::parse(payload);
+        assert_debug_snapshot!(tokens);
+    }
+
+    #[test]
+    fn tokenizer_string() {
+        let payload = r#""hola""#;
+        let tokens = Tokenizer::parse(payload);
+        assert_debug_snapshot!(tokens);
+    }
+
+    #[test]
+    fn tokenizer_string_with_scape() {
+        let payload = r#""ho\"la""#;
+        let tokens = Tokenizer::parse(payload);
+        assert_debug_snapshot!(tokens);
+    }
+
+    #[test]
+    fn tokenizer_variable() {
+        let payload = "FOO:BAZ";
+        let tokens = Tokenizer::parse(payload);
+        assert_debug_snapshot!(tokens);
+    }
+
+    #[test]
+    fn tokenizer_comment_and_variable() {
+        let payload = "# comment\nFOO:BAZ";
+        let tokens = Tokenizer::parse(payload);
+        assert_debug_snapshot!(tokens);
+    }
+
+    #[test]
+    fn tokenizer_variable_with_default_value() {
+        let payload = r#"FOO:BAZ="abc""#;
+        let tokens = Tokenizer::parse(payload);
+        assert_debug_snapshot!(tokens);
+    }
+
+    #[test]
+    fn tokenizer_number_1() {
+        let payload = r#"12"#;
+        let tokens = Tokenizer::parse(payload);
+        assert_debug_snapshot!(tokens);
+    }
+
+    #[test]
+    fn tokenizer_number_2() {
+        let payload = r#"12_123_456.123_456"#;
+        let tokens = Tokenizer::parse(payload);
+        dbg!(tokens);
+    }
+
+    #[test]
+    fn tokenizer_number_3() {
+        Tokenizer::parse("123");
+        Tokenizer::parse("1_23");
+        Tokenizer::parse("1_23.23");
+
+        assert!(std::panic::catch_unwind(|| {
+            Tokenizer::parse("12__23");
+        })
+        .is_err());
+        assert!(std::panic::catch_unwind(|| {
+            Tokenizer::parse("12_");
+        })
+        .is_err());
+        assert!(std::panic::catch_unwind(|| {
+            Tokenizer::parse("12_3._");
+        })
+        .is_err());
+        assert!(std::panic::catch_unwind(|| {
+            Tokenizer::parse("12_3.3_3.1");
+        })
+        .is_err());
+    }
+
+    #[test]
+    fn tokenizer_spaces() {
+       let tokens = Tokenizer::parse("foo : string = asd\nvar : int = \"234\"");
+       assert_debug_snapshot!(tokens);
+    }
 }
